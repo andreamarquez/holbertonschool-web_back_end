ac2e24c0a057ee225e17d66f5929fcd5329c1e9c
andreamarquez <andreamarquezg.95@gmail.com>
2025-04-25 10:24:02 +0200

    4-define_variables.py

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+This module defines variables with type annotations.
+
+The variables include:
+- an integer (`a`)
+- a floating-point number (`pi`)
+- a boolean (`i_understand_annotations`)
+- a string (`school`)
+"""
+
+a: int = 1
+"""
+a (int): An integer with the value 1.
+"""
+
+pi: float = 3.14
+"""
+pi (float): A floating-point number representing
+the value of π (approximately 3.14).
+"""
+
+i_understand_annotations: bool = True
+"""
+i_understand_annotations (bool): A boolean indicating
+understanding of type annotations.
+"""
+
+school: str = "Holberton"
+"""
+school (str): A string representing the name of
+the school, "Holberton".
+"""
