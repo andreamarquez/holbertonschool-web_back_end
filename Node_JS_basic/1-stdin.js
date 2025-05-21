5adb0722ba4b35bdd34295079c04efbe4969e308
andreamarquez <andreamarquezg.95@gmail.com>
2025-05-21 19:38:16 +0200

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,10 +6,5 @@ process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
-  process.exit(); // Exit the program after displaying the closing message
-});
-
-process.stdin.on('end', () => {
-  // This ensures the closing message is displayed for piped input
-  console.log('This important software is now closing');
+  process.stdin.destroy();
 });
