8a5175e8b51aedb880da686d6172dc4e317b961e
andreamarquez <andreamarquezg.95@gmail.com>
2025-05-21 19:40:02 +0200

    fx

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,6 +5,8 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
   process.stdin.destroy();
 });
